b00635443a4d7c3869fd62bc1d2c726868cfb9b0
yufw <yynyygy@gmail.com>
2019-02-07 20:00:50 +0800

    lots of random spheres

--- Program.cs
+++ Program.cs
@@ -54,32 +54,65 @@ namespace RayTracingInOneWeekend
                 return (1.0F - t) * new Vec3(1.0F, 1.0F, 1.0F) + t * new Vec3(0.5F, 0.7F, 1.0F);
             }
         }
+        static HitableList randomScene()
+        {
+            List<IHitable> list = new List<IHitable>();
+            list.Add(new Sphere(new Vec3(0, -1000, 0), 1000, new Lambertian(new Vec3(0.5F, 0.5F, 0.5F))));
+            Random r = new Random();
+            for (int a = -11; a < 11; a++)
+            {
+                for (int b = -11; b < 11; b++)
+                {
+                    float chooseMat = (float)r.NextDouble();
+                    Vec3 center = new Vec3((float)(a + 0.9 * r.NextDouble()), 0.2F, (float)(b + 0.9 * r.NextDouble()));
+                    if ((center - new Vec3(4, 0.2F, 0)).Length() > 0.9)
+                    {
+                        if (chooseMat < 0.8)
+                        {
+                            list.Add(new Sphere(center, 0.2F, new Lambertian(new Vec3((float)(r.NextDouble() * r.NextDouble()), (float)(r.NextDouble() * r.NextDouble()), (float)(r.NextDouble() * r.NextDouble())))));
+                        }
+                        else if (chooseMat < 0.95)
+                        {
+                            list.Add(new Sphere(center, 0.2F, new Metal(new Vec3((float)(0.5 * (1 + r.NextDouble())), (float)(0.5 * (1 + r.NextDouble())), (float)(0.5 * (1 + r.NextDouble()))), (float)(0.5 * r.NextDouble()))));
+                        }
+                        else
+                        {
+                            list.Add(new Sphere(center, 0.2F, new Dielectric(1.5F)));
+                        }
+                    }
+                }
+            }
+            list.Add(new Sphere(new Vec3(0, 1, 0), 1, new Dielectric(1.5F)));
+            list.Add(new Sphere(new Vec3(-4, 1, 0), 1, new Lambertian(new Vec3(0.4F, 0.2F, 0.1F))));
+            list.Add(new Sphere(new Vec3(4, 1, 0), 1, new Metal(new Vec3(0.7F, 0.6F, 0.5F), 0.0F)));
+            return new HitableList(list);
+        }
         static void Main(string[] args)
         {
-            int nx = 200;
-            int ny = 100;
-            int ns = 100;
+            int nx = 1200;
+            int ny = 800;
+            int ns = 10;
 
             Console.WriteLine("P3");
-            Console.WriteLine("200 100");
+            Console.WriteLine("1200 800");
             Console.WriteLine("255");
 
-            List<IHitable> list = new List<IHitable>();
-            list.Add(new Sphere(new Vec3(0, 0, -1), 0.5F, new Lambertian(new Vec3(0.1F, 0.2F, 0.5F))));
-            list.Add(new Sphere(new Vec3(0, -100.5F, -1), 100, new Lambertian(new Vec3(0.8F, 0.8F, 0.0F))));
-            list.Add(new Sphere(new Vec3(1, 0, -1), 0.5F, new Metal(new Vec3(0.8F, 0.6F, 0.2F), 0.0F)));
-            list.Add(new Sphere(new Vec3(-1, 0, -1), 0.5F, new Dielectric(1.5F)));
-            list.Add(new Sphere(new Vec3(-1, 0, -1), -0.45F, new Dielectric(1.5F)));
-            
+            // List<IHitable> list = new List<IHitable>();
+            // list.Add(new Sphere(new Vec3(0, 0, -1), 0.5F, new Lambertian(new Vec3(0.1F, 0.2F, 0.5F))));
+            // list.Add(new Sphere(new Vec3(0, -100.5F, -1), 100, new Lambertian(new Vec3(0.8F, 0.8F, 0.0F))));
+            // list.Add(new Sphere(new Vec3(1, 0, -1), 0.5F, new Metal(new Vec3(0.8F, 0.6F, 0.2F), 0.0F)));
+            // list.Add(new Sphere(new Vec3(-1, 0, -1), 0.5F, new Dielectric(1.5F)));
+            // list.Add(new Sphere(new Vec3(-1, 0, -1), -0.45F, new Dielectric(1.5F)));
+
             // float R = (float)Math.Cos(Math.PI/4);
             // list.Add(new Sphere(new Vec3(-R, 0, -1), R, new Lambertian(new Vec3(0, 0, 1))));
             // list.Add(new Sphere(new Vec3(R, 0, -1), R, new Lambertian(new Vec3(1, 0, 0))));
-            HitableList world = new HitableList(list);
+            HitableList world = randomScene();
 
-            Vec3 lookfrom = new Vec3(3, 3, 2);
-            Vec3 lookat = new Vec3(0, 0, -1);
-            float distToFocus = (lookfrom - lookat).Length();
-            float aperture = 2;
+            Vec3 lookfrom = new Vec3(13, 2, 3);
+            Vec3 lookat = new Vec3(0, 0, 0);
+            float distToFocus = 10;
+            float aperture = 0.1F;
             Camera cam = new Camera(lookfrom, lookat, new Vec3(0, 1, 0), 20, (float)nx / (float)ny, aperture, distToFocus);
             Random randObj = new Random();
             for (int j = ny - 1; j >= 0; j--)
